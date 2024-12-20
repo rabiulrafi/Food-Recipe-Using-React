c6faac2de3ed33885fed21d07aa7476fff4a72f8
rabiulrafi <rabiulrafi.cse.just@gmail.com>
2024-12-20 21:34:07 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Navbar } from './components/Navbar'
 import { Favourite } from './pages/Favourite/Favourite'
@@ -14,23 +14,23 @@ function App() {
   return (
     <>
       <div className='min-h-screen p-6 rounded-sm bg-slate-500 text-white text-lg'>
-      <Navbar/>
-      <Routes>
-        <Route
-        path='/'
-        element={
-          <Home/>
-        }
-        />
-        <Route path='/fav' element={<Favourite/>}/>
-        <Route
-         path='/recipe-item/:id'
-         element={
-           <Details/>
-         }
-        />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route
+            path='/Food-Recipe-Using-React'
+            element={
+              <Home />
+            }
+          />
+          <Route path='/fav' element={<Favourite />} />
+          <Route
+            path='/recipe-item/:id'
+            element={
+              <Details />
+            }
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </div>
 
     </>
